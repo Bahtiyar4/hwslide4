dd402f08eebd0ac281845f36ac558abb345953f0
Your Name <you@example.com>
2023-10-12 13:59:51 +0300

    second

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class HelloVistula {
+class HelloVistula {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
     }
